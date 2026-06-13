3ee211a805c650c3ded6b21745a75daa0ddc25d7
Coder. Naiem <126259025+Naiem548@users.noreply.github.com>
2026-06-14 01:04:01 +0600

    Query 7 Completed

--- Query.sql
+++ Query.sql
@@ -162,3 +162,18 @@ FROM
     Bookings
 WHERE 
     total_cost > (SELECT AVG(total_cost) FROM Bookings);
+
+
+
+
+--Query 7
+
+SELECT 
+    match_id, 
+    fixture, 
+    base_ticket_price
+FROM 
+    Matches
+ORDER BY 
+    base_ticket_price DESC
+LIMIT 2 OFFSET 1;
