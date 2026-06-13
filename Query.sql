6ec5f46ea97a19d7d0cd50ea748b19563247770e
Coder. Naiem <126259025+Naiem548@users.noreply.github.com>
2026-06-14 01:01:34 +0600

    Query 4 Completed

--- Query.sql
+++ Query.sql
@@ -117,3 +117,20 @@ FROM
     Bookings
 WHERE 
     payment_status IS NULL;
+
+
+
+
+--Query 4
+
+SELECT 
+    b.booking_id, 
+    u.full_name, 
+    m.fixture, 
+    b.total_cost
+FROM 
+    Bookings b
+INNER JOIN 
+    Users u ON b.user_id = u.user_id
+INNER JOIN 
+    Matches m ON b.match_id = m.match_id;
